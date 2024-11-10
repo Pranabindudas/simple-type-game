49b0a2fc588b8491cd0657b5c3f65bd76667789b
Pranabindudas <dasroki2003@gmail.com>
2024-11-10 02:54:21 -0800

    first commit

--- concept.js
+++ concept.js
@@ -7,11 +7,26 @@
 // playgroundsection.classList.remove('hidden');
 
 // }
-
-
-
-
-
+function handlekeybordbuttonpress(event){
+    const playerpressed = event.key;
+    console.log('playerpressed',playerpressed);
+    // get expect to press
+    const currentalphabateelement = document.getElementById('current-alphabate');
+    const currentalphabate = currentalphabateelement.innerText;
+    const exceptalpha = currentalphabate.toLowerCase();
+    console.log(playerpressed, exceptalpha)
+    // check correct or not
+    if(playerpressed===exceptalpha){
+        console.log('you are get a point');
+        console.log('you have pressed courectly', exceptalpha);
+        removebackgroundbyid(exceptalpha);
+        continuegame();
+    }
+    else{
+        console.log('you lost 1 point')
+    }
+}
+document.addEventListener('keyup', handlekeybordbuttonpress)
 
 
 
@@ -22,14 +37,21 @@ function play(){
     showelementbyid('playground-section')
     continuegame()
 }
+
+
+
+
 function continuegame(){
-    const alphabate = getrandomindex();
-    console.log('your random alphabte', alphabate);
-}
+    const alphabat = getrandomindex();
+    console.log('your random alphabte', alphabat);
+
+
 // set random alphabate
 const currentalphabateelement = document.getElementById('current-alphabate');
-currentalphabateelement.innerText = alphabate;
-
+currentalphabateelement.innerText = alphabat;
+// setbackground color
+setbackgroundcolorbyid(alphabat)
+}
 
 // optional
 function hideelementbyid(elementid){
@@ -40,15 +62,25 @@ function showelementbyid(elementid){
     const element = document.getElementById(elementid)
     element.classList.remove('hidden');
 }
+
+function setbackgroundcolorbyid(elementid){
+    const element =document.getElementById(elementid);
+    element.classList.add('bg-orange-400');}
+
+function removebackgroundbyid(elementid){
+    const element =document.getElementById(elementid);
+    element.classList.remove('bg-orange-400');
+
+}
 function getrandomindex(){
     // generate arendom alphabate
     const alphabatestring = 'abcdefghijklmnopqrstuvwxyz';
     const alphabates = alphabatestring.split('');
-    console.log(alphabates)
+    // console.log(alphabates)
     // get a random index 0 to 25
     const randomnumber = Math.random()*25;
     const index = Math.round(randomnumber);
-    const alphabate = alphabates[index];
+    const alphabat = alphabates[index];
     // console.log(index, alphabate);
-    return alphabate
+    return alphabat;
 }
